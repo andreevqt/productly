7d3417b271fce5c8986ec0e60c75fb8bbc18033e
Alex <qprquo@gmail.com>
2020-03-16 00:21:47 +0300

    Add news section

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,15 @@
 import $ from "jquery";
+import "slick-carousel";
 
 $(() => {
   const body = $(document.body);
   $(".b-header__toggle").click(() => {
     body.toggleClass("mobile-menu-is-shown");
   });
+  $(".news-slider").slick({
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: false,
+    dots: true,
+  })
 })
