7527b7ba231267c93b78738f453e6b3fd9ea7de8
qprquo <qprquo@gmail.com>
2020-03-16 10:27:37 +0300

    Refactoring

---
+++ README.md
@@ -0,0 +1,11 @@
+# Productly
+this is an implementation of sketch template provided by [Abdullah Un Noman ](https://dribbble.com/unnoman)
+
+```sh
+# install dependencies
+npm install
+# build 
+npm run build
+# local dev
+npm run dev
+```
--- src/js/app.js
+++ src/js/app.js
@@ -3,13 +3,30 @@ import "slick-carousel";
 
 $(() => {
   const body = $(document.body);
+
   $(".b-header__toggle").click(() => {
     body.toggleClass("mobile-menu-is-shown");
   });
+
   $(".news-slider").slick({
-    slidesToShow: 3,
-    slidesToScroll: 3,
     arrows: false,
     dots: true,
-  })
+    mobileFirst: true,
+    responsive: [
+      {
+        breakpoint: 540,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      }
+    ]
+  });
 })
